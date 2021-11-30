8fe2dcfe7f1b68212fd9851b4254d4556f53c5b5
Sarvesh Dakhane <Sarvesh.Dakhane@hotmail.com>
2021-11-30 08:11:35 +0100

    Intro of OpenMP

---
+++ u1Files/P1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <omp.h>
+
+//g++ -fopenmp myprog.cpp -o myprog
+
+/*
+Need to support OpenMP for Debugging this programm
+
+gdb (compiler Debuger Name for linux)
+--- g++ -g (debug option) -o0 (optional) -fopenmp
+---  ./barrier
+--- gdb ./barrier
+
+*/
